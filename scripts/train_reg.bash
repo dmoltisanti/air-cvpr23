747d9cd1702b537dd78787acabcdea6662624e0e
Davide Moltisanti <davidemoltisanti@gmail.com>
2023-03-06 17:28:22 +0000

    adds training scripts

---
+++ scripts/train_reg.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python run.py \
+./air/train_df.csv \
+./air/test_df.csv \
+./air/antonyms.csv \
+/home/davide/Desktop/temp/stack=16_stride=1 \
+/home/davide/Desktop/temp
---
+++ scripts/train_reg_fixed_d.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python run.py \
+./air/train_df.csv \
+./air/test_df.csv \
+./air/antonyms.csv \
+/home/davide/Desktop/temp/stack=16_stride=1 \
+/home/davide/Desktop/temp \
+--fixed_d
